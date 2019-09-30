4b5bc6dd88efbe21c13f37fd87e50d0be1d51786
Ben Vars <benrvars@gmail.com>
2019-09-29 22:36:03 -0400

    add knobs and notes

--- storybook/stories/Button/index.ios.js
+++ storybook/stories/Button/index.ios.js
@@ -2,16 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { TouchableHighlight } from 'react-native';
 
-export default function Button({ onPress, children }) {
-  return <TouchableHighlight onPress={onPress}>{children}</TouchableHighlight>;
+export default function Button({ onPress, children, disabled }) {
+	return (
+		<TouchableHighlight onPress={disabled ? null : onPress}>
+			{children}
+		</TouchableHighlight>
+	);
 }
 
 Button.defaultProps = {
-  children: null,
-  onPress: () => {},
+	children: null,
+	onPress: () => {}
 };
 
 Button.propTypes = {
-  children: PropTypes.node,
-  onPress: PropTypes.func,
+	children: PropTypes.node,
+	onPress: PropTypes.func
 };
--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -4,6 +4,7 @@ import { Text } from 'react-native';
 import { storiesOf } from '@storybook/react-native';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 
 // eslint-disable-next-line import/extensions
 import Button from './Button';
@@ -16,15 +17,22 @@ storiesOf('Welcome', module).add('to Storybook', () => (
 
 storiesOf('Button', module)
 	.addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
+	.addDecorator(withKnobs)
 	.add('with text', () => (
-		<Button onPress={action('clicked-text')}>
-			<Text>Hello Button</Text>
+		<Button
+			onPress={action('clicked-text')}
+			disabled={boolean('Disabled', false)}
+		>
+			<Text>{text('Label', 'Hello Storybook')}</Text>
 		</Button>
 	))
 	.add(
 		'with some emoji',
 		() => (
-			<Button onPress={action('clicked-emoji')}>
+			<Button
+				onPress={action('clicked-emoji')}
+				disabled={boolean('Disabled', false)}
+			>
 				<Text>😀 😎 👍 💯</Text>
 			</Button>
 		),
